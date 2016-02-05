ade27c8356cdf43113322db8c62b79d8bd80a557
grapswiz <ineeza@gmail.com>
2016-02-05 16:04:24 +0900

    tweetを解析してiOSに通知が来るようにしてみた

--- handle.go
+++ handle.go
@@ -5,7 +5,11 @@ import (
 	"fmt"
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/log"
+	"google.golang.org/appengine/urlfetch"
 	"encoding/json"
+	"strings"
+	"regexp"
+	"bytes"
 )
 
 type Tweet struct {
@@ -13,6 +17,12 @@ type Tweet struct {
 	Text		string
 	LinkToTweet	string
 	CreatedAt	string
+}
+
+type Values struct {
+	Value1		string        `json:"value1"`
+	Value2		string        `json:"value2"`
+	Value3		string        `json:"value3"`
 }
 
 func init() {
@@ -34,5 +44,49 @@ func trickplayHandler(w http.ResponseWriter, r *http.Request)  {
 		http.Error(w, "json parse error", 500)
 		log.Errorf(ctx, "json parse error: %v", err)
 	}
-	log.Infof(ctx, "%v %v %v %v", t.UserName, t.Text, t.LinkToTweet, t.CreatedAt)
+	//TODO 入荷した判定をする
+	if !strings.Contains(t.Text, "入荷しております") {
+		log.Infof(ctx, "no nyuuka")
+		return
+	}
+
+	log.Infof(ctx, "this is 入荷 tweet: " + t.Text)
+
+	//TODO 入荷した商品名を抽出する
+	//TODO 全ての、の後ろにスペースを挿入する
+	re := regexp.MustCompile("、?「(.+?)」|、?([^「」]+「.+?」)")
+	submatch := re.FindAllStringSubmatch(t.Text, -1)
+	var games []string
+	for _, v := range submatch {
+		if v[1] != "" {
+			games = append(games, v[1])
+		} else if v[2] != "" {
+			games = append(games, v[2])
+		}
+
+	}
+	log.Infof(ctx, "%v", games)
+
+	client := urlfetch.Client(ctx)
+
+	param := Values{
+		Value1: "あるボドゲ屋",
+		Value2: games[1],
+		Value3: games[2],
+	}
+	paramBytes, err := json.Marshal(param)
+	if err != nil {
+		log.Errorf(ctx, "%v", err)
+		return
+	}
+	if err != nil {
+		log.Errorf(ctx, "%v", err)
+		return
+	}
+	req.Header.Set("Content-Type", "application/json")
+	_, err = client.Do(req)
+	if err != nil {
+		log.Errorf(ctx, "%v", err)
+		return
+	}
 }
