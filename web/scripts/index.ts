dc86d8e24f6e2290dce75587182b37dbbe0a889d
grapswiz <ineeza@gmail.com>
2016-05-01 23:42:56 +0900

    add google analytics fixed #1

--- web/scripts/index.ts
+++ web/scripts/index.ts
@@ -1,15 +1,16 @@
 ///<reference path="../typings/angularjs/angular.d.ts"/>
 ///<reference path="../typings/angular-material/angular-material.d.ts"/>
+///<reference path="../typings/angulartics/angulartics.d.ts"/>
 
 import {App} from "./app";
 import {Header} from "./header";
 import {Settings} from "./settings";
 import {AogList} from "./aogList/aogList";
 
-const app = angular.module("app", ["ngComponentRouter", "ngMaterial", "angularMoment"]);
+const app = angular.module("app", ["ngComponentRouter", "ngMaterial", "angularMoment", "angulartics", "angulartics.google.analytics"]);
 
 app
-    .config(["$mdThemingProvider", "$locationProvider", ($mdThemingProvider:angular.material.IThemingProvider, $locationProvider:ng.ILocationProvider) => {
+    .config(["$mdThemingProvider", "$locationProvider", "$analyticsProvider", ($mdThemingProvider:angular.material.IThemingProvider, $locationProvider:ng.ILocationProvider, $analyticsProvider:angulartics.IAnalyticsServiceProvider) => {
         $mdThemingProvider
             .theme("default")
             .primaryPalette("grey", {
@@ -18,6 +19,9 @@ app
             .accentPalette("orange");
 
         // $locationProvider.html5Mode(true);
+
+        $analyticsProvider.firstPageview(true); /* Records pages that don't use $state or $route */
+        $analyticsProvider.withAutoBase(true);  /* Records full path */
     }])
 
     .component(App.name, App)
